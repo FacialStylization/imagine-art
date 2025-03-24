ab7b0dc56e1142710040efcf8937b1fb82e65ee9
neverbiasu <1751162157@qq.com>
2025-03-24 23:40:28 +0800

    feat: Update app.vue to use NuxtPage and add global styles

--- app.vue
+++ app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+@import '@fortawesome/fontawesome-svg-core/styles.css';
+
+:root {
+  --color-primary: #0ea5e9;
+  --color-primary-light: #7dd3fc;
+  --color-primary-dark: #0369a1;
+  --color-gray-600: #4b5563;
+  --color-gray-100: #f3f4f6;
+}
+</style>
